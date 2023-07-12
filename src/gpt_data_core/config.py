c7885ea091be396e0f367c4df80731c211b2bbf4
MikkelHansenAbtion <meh@abtion.com>
2023-07-12 10:44:15 +0200

    config: vector dimensions use correct env variable

--- src/gpt_data_core/config.py
+++ src/gpt_data_core/config.py
@@ -10,6 +10,6 @@ class Config:
         self.REDIS_PASSWORD = os.getenv("REDIS_PASSWORD") or None
         self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or None
         self.DEFAULT_DATA_PATH = os.getenv("DEFAULT_DATA_PATH") or None
-        self.VECTOR_DIMENSIONS = os.getenv("OPENAI_API_KEY") or 1536
+        self.VECTOR_DIMENSIONS = os.getenv("VECTOR_DIMENSIONS") or 1536
         self.DOC_PREFIX = os.getenv("DOC_PREFIX") or "doc:"
         self.INDEX_NAME = os.getenv("INDEX_NAME") or "index"
