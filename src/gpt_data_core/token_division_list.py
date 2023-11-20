2b6474c0f67ec75b4728200fb8b33486732b75b2
Sune Seifert <sse@abtion.com>
2023-11-20 14:06:46 +0100

    token_division_list: add division stats

--- src/gpt_data_core/token_division_list.py
+++ src/gpt_data_core/token_division_list.py
@@ -18,12 +18,26 @@ class TokenDivisionList:
         number_of_tokens_used = self.__num_tokens(text)
 
         if number_of_tokens_used < self.max_tokens:
-            return
+            return False
 
         divisions = -(-number_of_tokens_used // self.max_tokens)  # Ceiling division
         print(f"Number divisions: {divisions} for {file_path}")
+        return True
 
     def process_files(self, input_path):
-        print(f"Division list (tokenlimit: {self.max_tokens}):")
+        print(f"Division list:")
+        
+        totalCount = 0
+        processedCount = 0
+        
         for file_path in glob.glob(f"{input_path}/**/*.txt", recursive=True):
-            self.process_file(Path(file_path))
+            processed = self.process_file(Path(file_path))
+
+            totalCount += 1
+            if processed:
+                processedCount += 1
+
+        print(f"Model used:    {self.model}")
+        print(f"Tokenlimit:    {self.max_tokens}")
+        print(f"Total files:   {totalCount}")
+        print(f"To be divided: {processedCount}")
